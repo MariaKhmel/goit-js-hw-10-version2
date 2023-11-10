6a8dd7ea92b0b27cbfc2c7a41c49ac0f9d45af84
MariaKhmel <mariakhmel8@gmail.com>
2023-11-10 02:32:16 +0200

    film api

--- src/filmApi/fetchFilm.js
+++ src/filmApi/fetchFilm.js
@@ -1,16 +1,43 @@
 const API_KEY = 'f4fc383e5974e2ac547a6b655d2da3b0';
+const DEFAULT_PAGE = 1;
+let page = DEFAULT_PAGE;
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie'
 
+function resetPage() {
+    page = DEFAULT_PAGE;
+}
 
+const options = {
+    method:'get',
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNGZjMzgzZTU5NzRlMmFjNTQ3YTZiNjU1ZDJkYTNiMCIsInN1YiI6IjY1NGMxNmFhNDFhNTYxMzM2YTI0OWZkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xrML7A2wgEuY19W-c15nANg6x6o7f5jv_k3EA8kmHGo',
+        
+    }
+}
 
 function fetchFilmListByName(movieName) {
-    return fetch(`https://api.themoviedb.org/3/search/movie?query=${movieName}&api_key=${API_KEY}`).then(res => {
-        console.log(res);
+    const searchParams = new URLSearchParams({
+        query: movieName,
+        include_adult: false,
+        language: 'en-US',
+        page,
+    })
+    return fetch(`${BASE_URL}?${searchParams}`, options)
+        .then(res => {
         if (!res.ok) {
-            console.log(res);
             throw new Error(statusText);
         }
-        return res.json();
+            return res.json();
+    
+        }).then(data => {
+          
+            const filmsdata = {
+                data: data.results,
+                isLastPage: page === data.total_pages,
+            }
+            page += 1;
+            return filmsdata;
     })
 }
 
-export{fetchFilmListByName}
+export { fetchFilmListByName, resetPage }
--- src/filmApi/filmApi.js
+++ src/filmApi/filmApi.js
@@ -1,25 +1,45 @@
 import 'modern-normalize';
 import { createFilmsListMarkUp } from './filmListmarkUp.js';
-import { fetchFilmListByName } from './fetchFilm.js'
+import { fetchFilmListByName, resetPage } from './fetchFilm.js'
 
 const filmList = document.querySelector('.film-list');
-const form = document.querySelector('.searchbox')
+const form = document.querySelector('.searchbox');
+const loadMoreBtn = document.querySelector('.load-more')
 
+let movieName = '';
 form.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMoreBtnClick)
 
 
 function onFormSubmit(e) {
     e.preventDefault();
-    const movieName = e.target.elements.filmToSearch.value;
+    filmList.innerHTML = '';
+    movieName = e.target.elements.filmToSearch.value;
+    if (!movieName) {
+       return alert('Please eneter film name!');
+        
+    }
+    resetPage();
     fetchFilmListByName(movieName).then(list => {
-        if (!movieName) {
-            alert('empty')
+        if (list.isLastPage) {
+            loadMoreBtn.classList.remove('is-visible');
+        } else {
+            loadMoreBtn.classList.add('is-visible');
         }
-        filmList.insertAdjacentHTML('afterbegin', createFilmsListMarkUp(list.results))
-
+        
+        filmList.insertAdjacentHTML('beforeend', createFilmsListMarkUp(list.data))
     })
 
 }
 
 
+function onLoadMoreBtnClick(e) {
+    e.preventDefault();
+    fetchFilmListByName(movieName).then(list => {
+        filmList.insertAdjacentHTML('beforeend', createFilmsListMarkUp(list.data))
+        if (list.isLastPage) { 
+            loadMoreBtn.classList.remove('is-visible');
+}
 
+    })
+}
